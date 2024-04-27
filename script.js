65610da2789ba1cedb99e506a1adb96704226c2b
Diego Palomino <diegof.palomino@gmail.com>
2024-04-27 06:36:44 -0400

    Hemos hecho una Web de consulta de clima para uso de JS

---
+++ script.js
@@ -0,0 +1,52 @@
+const urlBase= `https://api.openweathermap.org/data/2.5/weather`
+const API_KEY= '19a661e09b86a3a7ea50d96a20bccda4'
+const diffKelvin = 273.15
+
+document.getElementById('searchButton').addEventListener('click', () => {
+    const city = document.getElementById('cityInput').value;
+    if(city){
+        //Llamar a la API para que nos de la informacion del clima
+        fetchweather(city)
+    }else{
+        alert('Ingrese una ciudad valida')
+    }
+})
+
+function fetchweather(city){
+    fetch(`${urlBase}?q=${city}&appid=${API_KEY}&lang=es`)
+    .then(data => data.json())
+    .then(data => weatherData(data))
+}
+
+function weatherData(data){
+     const divResponseData = document.getElementById('responseData') 
+     divResponseData.innerHTML = ''
+
+     const cityName = data.name
+     const countryName = data.sys.country
+     const temp = data.main.temp
+     const humidity = data.main.humidity
+     const description = data.weather[0].description
+     const icon = data.weather[0].icon
+
+     const cityInfo = document.createElement('h2')
+     cityInfo.textContent = `${cityName}, ${countryName}`
+
+     const tempInfo = document.createElement('p')
+     tempInfo.textContent = `La temperatura es: ${Math.floor(temp-diffKelvin)}°C`
+
+     const humidityInfo = document.createElement('p')
+     humidityInfo.textContent = `La humedad es del ${humidity}%`
+
+     const icoInfo = document.createElement('img')
+     icoInfo.src = `https://openweathermap.org/img/wn/${icon}@2x.png` //Verificar en la documentacion como mostrar una imagen
+
+     const descriptionInfo = document.createElement('p')
+     descriptionInfo.textContent = `La descripcion meteorologica es ${description}`
+
+     divResponseData.appendChild(cityInfo)
+     divResponseData.appendChild(tempInfo)
+     divResponseData.appendChild(humidityInfo)
+     divResponseData.appendChild(icoInfo)
+     divResponseData.appendChild(descriptionInfo)
+}
